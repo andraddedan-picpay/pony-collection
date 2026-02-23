32423179f9f3125f824a5f39f7a5c406797b1b72
Daniel Andrade <daniel.andrade@picpay.com>
2026-02-23 18:09:39 -0300

    fix(api): update initial schema

---
+++ api/src/database/migrations/1771880811124-InitialSchema.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitialSchema1771880811124 implements MigrationInterface {
+  name = 'InitialSchema1771880811124';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "ponies" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "isFavorite" boolean NOT NULL DEFAULT (0), "element" varchar NOT NULL, "personality" varchar NOT NULL, "talent" varchar NOT NULL, "summary" text NOT NULL, "imageUrl" varchar NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "ponies"`);
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
